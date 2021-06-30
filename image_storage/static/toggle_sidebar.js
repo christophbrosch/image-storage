28144f338fec2672b8e50e77283ca6225c8fa0a7
Christoph Brosch <rahon@pop-os.localdomain>
2021-06-30 18:35:59 +0200

    functioning sidebar with navbar on top level

--- image_storage/static/toggle_sidebar.js
+++ image_storage/static/toggle_sidebar.js
@@ -9,6 +9,11 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
+    // Setup the breakpoint variable
+    var breakpoint;
+    sidebarWrapper = document.body.querySelector('#sidebar-wrapper');
+    contentWrapper = document.body.querySelector('#content-wrapper');
+
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
@@ -16,11 +21,11 @@ window.addEventListener('DOMContentLoaded', event => {
         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
         //     document.body.classList.toggle('sb-sidenav-toggled');
         // }
+
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));   
         });
-    }
-
+    };
 });
